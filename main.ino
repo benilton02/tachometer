79aed999ea6a8a093ffe4b27dce435d0e6f2adba
Benilton <bsa.ele@uea.edu.br>
2021-06-23 22:51:45 -0500

    feat: left and right motor rotation

--- main.ino
+++ main.ino
@@ -1,88 +1,107 @@
 #include <LiquidCrystal_I2C.h>
 #include <NewPing.h>
 
-#define pushButton 16
+#define pushButton 11
+
+#define velocity 6 
+#define mla 3
+#define mlb 5
+
 #define address 0x27       
 #define row 2
 #define column 16
 
-//#define triger 11
+#define triggerPin  13  
+#define echoPin     12  
 #define safetyDistance 5
-// #define ultrasonicPin 2 // interrupção 0
 
 
-#define opticalPin 3 // interrupção 1
-
-
-// NewPing ultrasonic(, maxDistance)
 LiquidCrystal_I2C lcd(address, column, row);
+NewPing sonar(triggerPin, echoPin, safetyDistance);
 
-volatile int currentDistance,rpm;
-volatile int statusPushButton = 0;
-volatile int timeRpm = 0;
-volatile int cont = 0;
-volatile int myStatus = 0;
-unsigned long int buttonTime = 0;
-
-
-void setup(){    
-    setLcd();
-
-    pinMode(opticalPin, INPUT);
-    attachInterrupt(digitalPinToInterrupt(opticalPin), revolution, RISING);
+volatile unsigned int currentDistance;
+volatile unsigned long int buttonTime = 0;
+volatile unsigned int statusPushButton = 0;
+volatile unsigned int option = 0;
 
 
-    // pinMode(ultrasonicPin, INPUT);
-    // attchInterrupt(
-    //     digitalPinToInterrupt(ultrasonicPin),
-    //     stopMotor,
-    //     RISING
-    // );
-
+void setup() {
+  Serial.begin(115200);
+  setPin();
+  stopMotor();
+  setLcd();
 }
 
-void loop(){
-    if (stopMotor){
-        checkButton();
 
-        timeRpm = millis();
-        if (millis() - timeRpm >= 500){
-            showRotation(); 
-            timeRpm = millis();
-        }
-
-
-    }
+void loop() {
+  if(checkSafetyDistance()){
+    checkButton();
+    motorStatus();
+  }
+  else
+    stopMotor();
 }
 
-    
-void checkButton(){
-    statusPushButton = digitalRead(pushButton);
-    if(statusPushButton){
-      if (millis() - buttonTime >= 5000){
-        myStatus++;
-        buttonTime = millis();
-      }
-    }
+
+void leftRotation(){
+  digitalWrite(mla, LOW);
+  digitalWrite(mlb, HIGH);
+  analogWrite(velocity, 255);
 }
 
-void showRotation(){
-    rpm = cont*3;
-    cont = 0;
-    
-    lcd.print("");
-    lcd.setCursor(0, 0);
-    lcd.print("RPM: ");
-    lcd.print(rpm);
-    lcd.setCursor(0, 1);  
-    
-    // if (alguma_coisa)
-    //     lcd.print("Rotation: Right");
 
-    // else{
-    //     lcd.print("Rotation: Left");
-    // }
+void rightRotation(){
+  digitalWrite(mla, HIGH);
+  digitalWrite(mlb, LOW);
+  analogWrite(velocity, 255);
 }
+
+
+void stopMotor(){
+  digitalWrite(mla, LOW);
+  digitalWrite(mlb, LOW);
+  analogWrite(velocity, 255);
+}
+
+
+void setPin(){
+  pinMode(pushButton, INPUT_PULLUP);
+  pinMode(velocity, OUTPUT);
+  pinMode(mla, OUTPUT);
+  pinMode(mlb, OUTPUT);
+}
+
+
+void motorStatus(){
+  switch(option){
+  
+  case 0:
+    stopMotor(); 
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print("Motor: STOP!");
+    break;
+
+  case 1:
+    rightRotation();
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print("Rotation: RIGHT");
+    break;
+
+  case 2:
+    leftRotation();
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print("Rotation: LEFT");
+    break;
+
+  case 3:
+    option = 0;
+    break;
+  }
+}
+
 
 void setLcd(){
   lcd.init();
@@ -90,12 +109,21 @@ void setLcd(){
   lcd.clear(); 
 }
 
-void revolution(){
-    cont++;
+
+void checkButton(){
+  statusPushButton = digitalRead(pushButton);
+  if(!statusPushButton){
+    if (millis() - buttonTime >= 3000){
+      option++;
+      buttonTime = millis();
+    }
+  }
 }
 
-bool stopMotor(){
-    //determinar a distancia medida pelo senso ultrassonico
-    currentDistance <= safetyDistance ? true : false;
+
+bool checkSafetyDistance(){
+  delay(50);                     
+  currentDistance = sonar.ping_cm();
+  Serial.println(currentDistance);
+  return currentDistance != 0 ? false : true;
 }
-    
